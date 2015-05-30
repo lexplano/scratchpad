7d8f6f4e0c95f5a27b107e0d7846f59cdc202a05
Dominykas Blyžė <hello@dominykas.com>
2015-05-30 17:43:14 +0300

    cleanup; square logo

---
+++ logo/create-logo.js
@@ -0,0 +1,15 @@
+var TextToSVG = require("text-to-svg").TextToSVG,
+	fs = require("fs");
+
+var textToSVG = new TextToSVG("JosefinSans-Thin.ttf");
+
+var logoSvg = textToSVG
+	.getSVG("lexplano", { fontSize: 600, attributes: { transform: "translate(37,480)"} })
+	.replace("><path",'width="1920" height="600" viewBox="0 0 1920 600"><path');
+
+var squareLogoSvg = textToSVG
+	.getSVG("lexplano", { fontSize: 600, attributes: { transform: "translate(37,1140)"} })
+	.replace("><path",'width="1920" height="1920" viewBox="0 0 1920 1920"><path');
+
+fs.writeFileSync("logo.svg", logoSvg);
+fs.writeFileSync("logo-square.svg", squareLogoSvg);
--- logo/create-path.js
+++
@@ -1,7 +0,0 @@
-var TextToSVG = require("text-to-svg").TextToSVG;
-
-var logoSvg = (new TextToSVG("JosefinSans-Thin.ttf"))
-	.getSVG("lexplano", { fontSize: 600, attributes: { transform: "translate(37,480)"} })
-	.replace("><path",'width="1920" height="600" viewBox="0 0 1920 600"><path');
-
-require("fs").writeFileSync("logo-path.svg", logoSvg);
